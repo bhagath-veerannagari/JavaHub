43e5bfe56d48048bb5d14664ea1eedead0aaec5a
bhagath-veerannagari <b.veerannagari@gmail.com>
2025-02-05 13:09:52 -0600

    added an array operation from leetcode

---
+++ JavaCore/src/main/java/com/bhaggie/coreBasics/coreOperations/ArrayOperations.java
@@ -0,0 +1,28 @@
+package main.java.com.bhaggie.coreBasics.coreOperations;
+
+public class ArrayOperations {
+
+    public static int sumOfElementsInAnArray (int inputArray[]) {
+        int sum = 0;
+        for(int i=0; i<inputArray.length; i++) {
+            sum = sum + inputArray[i];
+        } return sum;
+    }
+
+    public static double avgOfElementsInAnArray (int inputArray[]) {
+        return sumOfElementsInAnArray(inputArray)/inputArray.length;
+    }
+
+    public static int[] returnIndicesOftwoNumbersAddingUpToTarget (int[] inputArray, int target) {
+        int[] resultantArray = new int[0];
+        for (int i = 0; i<inputArray.length; i++) {
+            for (int j=i+1; j<inputArray.length; j++) {
+                if (inputArray[i] + inputArray[j] == target) {
+                    resultantArray = new int[]{i, j};
+                }
+            }
+        } return resultantArray;
+    }
+
+
+}
